0f6a8aaedf818c3832151da22b989fe9d2959f03
muhammad-hamza <Muh-Hamza-99>
2022-06-19 22:37:56 +0300

    Setting up basic express app, sessions, cors and passport

---
+++ server/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config({ path: "./config.env" });
+
+const express = require("express");
+const app = express();
+
+const passport = require("passport");
+
+const session = require("express-session");
+
+const CORS = require("cors");
+
+app.use(CORS({ credentials: true, origin: process.env.CLIENT_URL }));
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET, 
+    cookie: { 
+        secure: process.env.NODE_ENV === "production" ? "true" : "auto",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    },
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, console.log(`Server is up and running on port ${PORT}...`));
