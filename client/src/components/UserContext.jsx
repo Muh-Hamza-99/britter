1bcd8914dfa9b86ffa71269eb5ee893a95abc974
muhammad-hamza <Muh-Hamza-99>
2022-06-20 11:35:04 +0300

    Getting user data and cookie everytime page loads

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext({ loggedIn: false });
 
 const Context = ({ children }) => {
     const [user, setUser] = useState(() => ({ loggedIn: false }));
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}`, { credentials: "include" })
+            .then(response => response.json())
+            .then(data => setUser({ ...data }));
+        return () => {
+            cleanup
+        };
+    }, []);
     return <UserContext.Provider value={user}>{children}</UserContext.Provider>
 };
 
