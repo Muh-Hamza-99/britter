a960458dcb12139e9085b4f2f6af78248711e68d
muhammad-hamza <Muh-Hamza-99>
2022-06-20 19:57:50 +0300

    Refactoring repetitive links in navbar in general component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,22 +10,32 @@ const Navbar = ({ setModalOpen }) => {
     <StyledNavbar>
         <HamburgerButton className="fas fa-bars" onClick={() => setNavOpen(currentValue => !currentValue) } />
         <MobileMenu open={navOpen}>
-            <Link onClick={() => setNavOpen(false)} to="/"><h1>Social Media</h1></Link>
-            <Link onClick={() => setNavOpen(false)} to="/"><p>Feed</p></Link>
-            <Link onClick={() => setNavOpen(false)} to="/account"><p>Account</p></Link>
-            <PostModalButton className="fas fa-plus" onClick={() => {
-                setNavOpen(false);
-                setModalOpen(currentValue => !currentValue);
-            }}/>
+            <Links setNavOpen={setNavOpen} setModal={setModal} />
         </MobileMenu>
         <DesktopMenu>
-            <Link to="/"><h1>Social Media</h1></Link>
-            <Link to="/"><p>Feed</p></Link>
-            <Link to="/account"><p>Account</p></Link>
-            <PostModalButton className="fas fa-plus" />
+          <Links setNavOpen={null} setModal={setModal} />
         </DesktopMenu>
     </StyledNavbar>
   );
 };
 
-export default Navbar
+export default Navbar;
+
+const Links = ({ setNavOpen, setModalOpen }) => (
+  <>
+    <Link to="/" onClick={() => {if (setNavOpen) setNavOpen(false);}}>
+      <h1>Social Media</h1>
+    </Link>
+    <Link to="/" onClick={() => {if (setNavOpen) setNavOpen(false);}}>
+      <p>Feed</p>
+    </Link>
+    <Link to="/account" onClick={() => { if (setNavOpen) setNavOpen(false); }}>
+      <p>Account</p>
+    </Link>
+    <PostModalButton onClick={() => {
+        if (setNavOpen) setNavOpen(false);
+        setModalOpen(currentValue => !currentValue);
+      }}
+      className="fas fa-plus" />
+  </>
+);
