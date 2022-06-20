bc9f5a70eaf02aec8b279119af01bb6dee6ecf86
muhammad-hamza <Muh-Hamza-99>
2022-06-20 11:38:49 +0300

    Fix: fetch url

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -5,7 +5,7 @@ export const UserContext = createContext({ loggedIn: false });
 const Context = ({ children }) => {
     const [user, setUser] = useState(() => ({ loggedIn: false }));
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}`, { credentials: "include" })
+        fetch(`${process.env.REACT_APP_SERVER_URL}/account`, { credentials: "include" })
             .then(response => response.json())
             .then(data => setUser({ ...data }));
     }, []);
